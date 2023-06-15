7228e0ccf537573f1fbb4805bcb030c0d4b662c8
kgathala <kgathala1@icloud.com>
2023-06-15 16:42:02 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
 /**
  * main - Entry point of the program
  *
@@ -12,17 +11,18 @@
  */
 int main(void)
 {
-    int fd;
-    ssize_t len;
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int fd;
+	ssize_t len;
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-    len = strlen(message);
-    fd = open("/dev/stderr", O_WRONLY);
-    if (fd != -1)
-    {
-        write(fd, message, len);
-        close(fd);
-    }
-    return (1);
+	len = strlen(message);
+	fd = open("/dev/stderr", O_WRONLY);
+	if (fd != -1)
+	{
+		write(fd, message, len);
+		close(fd);
+	}
+
+	return (1);
 }
 
