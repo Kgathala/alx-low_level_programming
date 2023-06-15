3047c119656f4f13baf8b0b602015d3b2f1a87f6
kgathala <kgathala1@icloud.com>
2023-06-15 16:57:53 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,5 @@
 #include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <stdio.h>
 
 /**
  * main - Entry point of the program
