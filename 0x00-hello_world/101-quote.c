ea05b1062cc41d6b02cd8d09772ed06abc1205a0
kgathala <kgathala1@icloud.com>
2023-06-15 16:29:47 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,27 @@
-#include <stdio.h>                                                                                                                                                /**                                                                                                                                                                * main - Entry point of the program
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/**
+ * main - Entry point of the program
  *
- * Return: Always 0 (indicating success)
+ * Return: Always 1 (indicating failure)
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
+    int fd;
+    ssize_t len;
+    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-    return (0);
+    len = strlen(message);
+    fd = open("/dev/stderr", O_WRONLY);
+    if (fd != -1)
+    {
+        write(fd, message, len);
+        close(fd);
+    }
+    return (1);
 }
+
