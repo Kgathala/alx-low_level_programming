7bdef08597e2916b3870c787f9f199f921329e88
kgathala <kgathala1@icloud.com>
2023-06-21 05:08:11 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,11 @@
-[20:25, 20/06/2023] +27 83 442 6790: #include "main.h"
+#include "main.h"
 
 /**
- * print_sign - Prints the sign of a number.
- * @n: The number of which the sign will be printed.
- *
- * Return: 1 if the number is greater than zero,
- *         0 if the number is zero,
- *         -1 if the number is less than zero.
+ * print_sign - prints the sign of a number
+ * @n: the int to check
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
@@ -14,31 +13,13 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
+	} else if (n == 0)
+}
+		_putchar(48);
 		return (0);
-	}
-	else
+	} else if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
 	}
-}
-
-#include "main.h"
-
-/**
- * _abs - Computes the absolute value of an integer.
- * @n: The integer to be computed.
- *
- * Return: The absolute value of the integer.
- */
-int _abs(int n)
-{
-	if (n >= 0)
-		return (n);
-	else
-		return (-n);
+		return (-1);
 }
